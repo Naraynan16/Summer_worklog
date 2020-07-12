7d8adf7bdf6cb74f7ef7faae5afc7098083c2083
Naraynan16 <sathya.bfx@gmail.com>
2020-07-12 17:02:11 +0530

    refgene_introns

---
+++ exercises/RefGene_Data/introns.R
@@ -0,0 +1,26 @@
+codingTx<-read.csv(file ="C:/Users/Narayanan/Desktop/summer_worklog/coding_transcripts.csv",header = T)
+
+inStart <- list()
+inEnd <- list() 
+refgene_introns <- data.frame()
+k=1
+for (i in 1:dim(codingTx)[1]){
+  l <- 1
+  if (codingTx[i, 9] > 1){
+    
+      exStart = strsplit(x = codingTx[i, 10], split = ",")
+      exEnd = strsplit(x= codingTx[i, 11], split = "," )
+    
+ for (j in 1:codingTx[i, 9]-1){
+       
+ 
+   if(isTRUE(exEnd[[1]][j] != exStart[[1]][j+1])){
+    
+    refgene_introns[k,1:7]<- cbind(codingTx[i,2:4],paste0("intron_",l),exEnd[[1]][j],exStart[[1]][j+1],codingTx[i,13])
+      l <- l+1
+      k <- k+1
+   }
+}
+}
+}
+write.csv(x = refgene_introns,file = "refgene_introns.csv",row.names = F)
--- exercises/RefGene_Data/utr_extract.R
+++ exercises/RefGene_Data/utr_extract.R
@@ -10,43 +10,43 @@ for (i in 1:dim(codingTx)[1])
     
     if (codingTx[i,5] == codingTx[i,7] && codingTx[i,7] == exStart[[1]][1]){         
       
-      utr1[i, ] <- rbind(cbind("5UTR ", "-", "-", codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i, ] <- rbind(cbind("5UTR ", "-", "-", codingTx[i,2:4], codingTx[i,13]))
       
     }
     
     else if (codingTx[i,5] == codingTx[i,7] && codingTx[i,7] != exStart[[1]][1]){ 
       
-      utr1[i,1:7] <- rbind(cbind("5UTR ",codingTx[i,5], exStart[[1]][1], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i,1:7] <- rbind(cbind("5UTR ",codingTx[i,5], exStart[[1]][1], codingTx[i,2:4], codingTx[i,13]))
     }
     
     else if(codingTx[i,5] != codingTx[i,7] && codingTx[i,7] == exStart[[1]][1]){
       
-      utr1[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,5], codingTx[i,7], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,5], codingTx[i,7], codingTx[i,2:4], codingTx[i,13]))
     }
     
     else if(codingTx[i,5] != codingTx[i,7] && codingTx[i,5] == exStart[[1]][1]){
       
-      utr1[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,5], codingTx[i,7], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,5], codingTx[i,7], codingTx[i,2:4], codingTx[i,13]))
     } 
     
     if (codingTx[i,6] == codingTx[i,8] && codingTx[i,8] == exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i, ] <- rbind(cbind("3UTR ", "-", "-", codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i, ] <- rbind(cbind("3UTR ", "-", "-", codingTx[i,2:4], codingTx[i,13]))
     }
     
     else if (codingTx[i,6] == codingTx[i,8] && codingTx[i,8] != exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i,1:7] <- rbind(cbind("3UTR ", exEnd[[1]][codingTx[i,9]], codingTx[i,5], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i,1:7] <- rbind(cbind("3UTR ", exEnd[[1]][codingTx[i,9]], codingTx[i,5], codingTx[i,2:4], codingTx[i,13]))
     }
     
     else if(codingTx[i,6] != codingTx[i,8] && codingTx[i,8] == exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,8], codingTx[i,6], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,8], codingTx[i,6], codingTx[i,2:4], codingTx[i,13]))
     }
     
     else if(codingTx[i,6] != codingTx[i,8] && codingTx[i,6] == exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,8], codingTx[i,6], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,8], codingTx[i,6], codingTx[i,2:4], codingTx[i,13]))
     }
   }
   
@@ -54,48 +54,47 @@ for (i in 1:dim(codingTx)[1])
     
     if (codingTx[i,5] == codingTx[i,7] && codingTx[i,7] == exStart[[1]][1]){ 
       
-      utr1[i, ] <- rbind(cbind("3UTR ", "-", "-", codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i, ] <- rbind(cbind("3UTR ", "-", "-", codingTx[i,2:4], codingTx[i,13]))
     }
     
     else if (codingTx[i,5] == codingTx[i,7] && codingTx[i,7] != exStart[[1]][1]){
       
-      utr1[i,1:7] <- rbind(cbind("3UTR ", codingTx[i,5],exStart[[1]][1], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i,1:7] <- rbind(cbind("3UTR ", codingTx[i,5],exStart[[1]][1], codingTx[i,2:4], codingTx[i,13]))
     }
     
     
     else if(codingTx[i,5] != codingTx[i,7] && codingTx[i,7] == exStart[[1]][1]){
       
-      utr1[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,5], codingTx[i,7], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,5], codingTx[i,7], codingTx[i,2:4], codingTx[i,13]))
     }
     
     
     else if(codingTx[i,5] != codingTx[i,7] && codingTx[i,5] == exStart[[1]][1]){
       
-      utr1[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,5], codingTx[i,7], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr1[i,1:7] <- rbind(cbind("3UTR ",codingTx[i,5], codingTx[i,7], codingTx[i,2:4], codingTx[i,13]))
     }
     
     if (codingTx[i,6] == codingTx[i,8] && codingTx[i,8] == exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i, ] <- rbind(cbind("5UTR ", "-", "-", codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i, ] <- rbind(cbind("5UTR ", "-", "-", codingTx[i,2:4], codingTx[i,13]))
     }  
     else if (codingTx[i,6] == codingTx[i,8] && codingTx[i,8] != exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i,1:7] <- rbind(cbind("5UTR ", exEnd[[1]][codingTx[i,9]], codingTx[i,5], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i,1:7] <- rbind(cbind("5UTR ", exEnd[[1]][codingTx[i,9]], codingTx[i,5], codingTx[i,2:4], codingTx[i,13]))
     }
     else if(codingTx[i,6] != codingTx[i,8] && codingTx[i,8] == exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,8], codingTx[i,6], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,8], codingTx[i,6], codingTx[i,2:4], codingTx[i,13]))
     }
     else if(codingTx[i,6] != codingTx[i,8] && codingTx[i,6] == exEnd[[1]][codingTx[i,9]]){
       
-      utr2[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,8], codingTx[i,6], codingTx[i,3:4], codingTx[i,13], codingTx[i,2]))
+      utr2[i,1:7] <- rbind(cbind("5UTR ", codingTx[i,8], codingTx[i,6], codingTx[i,2:4], codingTx[i,13]))
     }
   }
 }
 
-colnames(utr1) <- c("type","start","end","chrom","strand","geneName","transcript_id")
-colnames(utr2) <- c("type","start","end","chrom","strand","geneName","transcript_id")
-
-all_utr <- merge(x = utr1,y = utr2)
+colnames(utr1) <- c("type","start","end","transcript_id","chrom","strand","geneName")
+colnames(utr2) <- c("type","start","end","transcript_id","chrom","strand","geneName")
+all_utr <- merge(x = utr1,y = utr2, by="chrom")
 colnames(all_utr) <- c("chrom","strand","geneName","transcript_id","utr.1","start.1","end.1","utr.2","start.2","end.2")
 write.csv(x = all_utr, file = "all_utr.csv", row.names = F)
